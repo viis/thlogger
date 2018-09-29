4e0de9318f8ded07af25b93602771ef3766f7d7f
Vignir Isberg <vigniri@gmail.com>
2018-09-29 21:01:09 +0200

    move  db connection init and writes into separate functions with retry
    this makes the connection and writing processes more resilient to connection problems

--- thlogger.py
+++ thlogger.py
@@ -6,6 +6,8 @@ import json
 from argparse import ArgumentParser
 import Adafruit_DHT
 from influxdb import InfluxDBClient
+from tenacity import retry, retry_if_exception_type, wait_fixed
+from requests.exceptions import ConnectionError
 
 
 class THLogger:
@@ -14,6 +16,9 @@ class THLogger:
                2302: Adafruit_DHT.AM2302}
 
     def __init__(self, args):
+        # container for the measurements
+        self.measurements = []
+
         # read config file
         self.CONFIG_FILE = args.CONFIG_FILE
         if os.path.exists(self.CONFIG_FILE):
@@ -40,15 +45,58 @@ class THLogger:
         self.logger.info('LOCATION %s', self.LOCATION)
 
         # init influxdb client
-        self.client = InfluxDBClient(self.HOST, self.PORT)
-        databases = [d.get('name') for d in self.client.get_list_database()]
-        self.logger.debug('AVAILABLE DATABASES %s', databases)
+        self.client = None
+        self.logger.info('INIT InfluxDB connection')
+        self.init_db_connection()
+
+    @retry(retry=retry_if_exception_type(ConnectionError), wait=wait_fixed(10))
+    def init_db_connection(self):
+        databases = []
+        if not self.client:
+            self.logger.debug('CONNECTING TO %s:%s', self.HOST, self.PORT)
+            self.client = InfluxDBClient(self.HOST, self.PORT)
+            databases = [d.get('name') for d in self.client.get_list_database()]
+            self.logger.debug('AVAILABLE DATABASES %s', databases)
+
         if self.DATABASE not in databases:
             self.client.create_database(self.DATABASE)
             self.logger.info('CREATED DATABASE %s', self.DATABASE)
         self.client.switch_database(self.DATABASE)
-        self.logger.info('INIT InfluxDB connection')
         self.logger.info('USE DATABASE %s', self.DATABASE)
+
+    def write_measurements(self):
+        # write measurements to db
+        try:
+            for measurement in self.measurements:
+                json_body = [
+                    {
+                        "measurement": "temperature",
+                        "tags": {
+                            "location": self.LOCATION
+                        },
+                        "time": str(measurement['timestamp']),
+                        "fields": {
+                            "value": measurement['temperature']
+                        }
+                    },
+                    {
+                        "measurement": "humidity",
+                        "tags": {
+                            "location": self.LOCATION
+                        },
+                        "time": str(measurement['timestamp']),
+                        "fields": {
+                            "value": measurement['humidity']
+                        }
+                    }
+                ]
+                self.logger.debug('JSON: %s', json_body)
+                self.client.write_points(json_body)
+                self.logger.info('Temp: %d C, hum: %d %%', measurement['temperature'], measurement['humidity'])
+            self.logger.debug('Wrote %d measurement(s)' % len(self.measurements))
+            self.measurements = []
+        except Exception:
+            self.logger.warning('FAILED TO WRITE MEASUREMENTS')
 
     def work(self):
         while True:
@@ -57,33 +105,16 @@ class THLogger:
                                          for reading in Adafruit_DHT.read_retry(self.SENSOR, self.GPIO_PIN)]
                 if humidity is not None and temperature is not None:
                     ts = datetime.datetime.now(datetime.timezone.utc)
-                    json_body = [
-                        {
-                            "measurement": "temperature",
-                            "tags": {
-                                "location": self.LOCATION
-                            },
-                            "time": str(ts),
-                            "fields": {
-                                "value": temperature
-                            }
-                        },
-                        {
-                            "measurement": "humidity",
-                            "tags": {
-                                "location": self.LOCATION
-                            },
-                            "time": str(ts),
-                            "fields": {
-                                "value": humidity
-                            }
-                        }
-                    ]
-                    self.logger.debug('JSON: %s', json_body)
-                    self.client.write_points(json_body)
-                    self.logger.info('Temp: %d C, hum: %d %%', temperature, humidity)
+                    measurement = {
+                        'timestamp': ts,
+                        'temperature': temperature,
+                        'humidity': humidity,
+                    }
+                    self.logger.debug('MEASURED %s' % measurement)
+                    self.measurements.append(measurement)
+                    self.write_measurements()
                 else:
-                    self.logger.warning('Failed to get a reading')
+                    self.logger.warning('FAILED TO GET A READING')
             except (KeyboardInterrupt, SystemExit):
                 raise
             except Exception as e:
